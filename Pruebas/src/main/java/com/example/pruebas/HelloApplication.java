b060283869bafa77ff925a288a9178a02a34bfd8
Jordi <jordivano17@gmail.com>
2021-10-13 17:39:03 +0200

    Actividad 1.1
    Actividad 1.1

--- Pruebas/src/main/java/com/example/pruebas/HelloApplication.java
+++ Pruebas/src/main/java/com/example/pruebas/HelloApplication.java
@@ -1,23 +1,122 @@
+//Activitat 1.1. Crea un tauler d’escacs com el que es mostra en la pantalla.
+//Tens més informació sobre l’activitat en el moodle.
+//Amb el que s'ha vist fins al moment en classe, has de crear una aplicació que ens dibuixe un tauler d'escacs.
+//Per aquesta tasca l'únic que necessitem és saber crear una escena i afegir-li rectangles.
+//Pots ajudar-te de l'exemple que s'ha vist en les transparències.
+//Les característiques que ha de tenir cada rectangle és:
+//La grandària dels quadrats ha de ser de 50.
+//Pots triar el color que vulgues.
+//Pots veure un exemple a continuació:
+
 package com.example.pruebas;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.lang.reflect.Array;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage escacs) throws IOException{
+        try {
+
+        escacs.setTitle("Practica1.1: Escacs");
+
+        Array[][] total = new Array[8][8];
+
+        Rectangle rectangle1= rectanguloColor1(0,0);
+        Rectangle rectangle2= rectanguloColor2(0,50);
+        Rectangle rectangle3= rectanguloColor1(0,100);
+        Rectangle rectangle4= rectanguloColor2(0,150);
+        Rectangle rectangle5= rectanguloColor1(0,200);
+        Rectangle rectangle6= rectanguloColor2(0,250);
+        Rectangle rectangle7= rectanguloColor1(0,300);
+        Rectangle rectangle8= rectanguloColor2(0,350);
+        Rectangle rectangle9= rectanguloColor1(100,0);
+        Rectangle rectangle10= rectanguloColor2(100,50);
+        Rectangle rectangle11= rectanguloColor1(100,100);
+        Rectangle rectangle12= rectanguloColor2(100,150);
+        Rectangle rectangle13= rectanguloColor1(100,200);
+        Rectangle rectangle14= rectanguloColor2(100,250);
+        Rectangle rectangle15= rectanguloColor1(100,300);
+        Rectangle rectangle16= rectanguloColor2(100,350);
+        Rectangle rectangle17= rectanguloColor1(200,0);
+        Rectangle rectangle18= rectanguloColor2(200,50);
+        Rectangle rectangle19= rectanguloColor1(200,100);
+        Rectangle rectangle20= rectanguloColor2(200,150);
+        Rectangle rectangle21= rectanguloColor1(200,200);
+        Rectangle rectangle22= rectanguloColor2(200,250);
+        Rectangle rectangle23= rectanguloColor1(200,300);
+        Rectangle rectangle24= rectanguloColor2(200,350);
+        Rectangle rectangle25= rectanguloColor1(300,0);
+        Rectangle rectangle26= rectanguloColor2(300,50);
+        Rectangle rectangle27= rectanguloColor1(300,100);
+        Rectangle rectangle28= rectanguloColor2(300,150);
+        Rectangle rectangle29= rectanguloColor1(300,200);
+        Rectangle rectangle30= rectanguloColor2(300,250);
+        Rectangle rectangle31= rectanguloColor1(300,300);
+        Rectangle rectangle32= rectanguloColor2(300,350);
+            Rectangle rectangle33= rectanguloColor2(50,0);
+            Rectangle rectangle34= rectanguloColor1(50,50);
+            Rectangle rectangle35= rectanguloColor2(50,100);
+            Rectangle rectangle36= rectanguloColor1(50,150);
+            Rectangle rectangle37= rectanguloColor2(50,200);
+            Rectangle rectangle38= rectanguloColor1(50,250);
+            Rectangle rectangle39= rectanguloColor2(50,300);
+            Rectangle rectangle40= rectanguloColor1(50,350);
+            Rectangle rectangle41= rectanguloColor2(150,0);
+            Rectangle rectangle42= rectanguloColor1(150,50);
+            Rectangle rectangle43= rectanguloColor2(150,100);
+            Rectangle rectangle44= rectanguloColor1(150,150);
+            Rectangle rectangle45= rectanguloColor2(150,200);
+            Rectangle rectangle46= rectanguloColor1(150,250);
+            Rectangle rectangle47= rectanguloColor2(150,300);
+            Rectangle rectangle48= rectanguloColor1(150,350);
+            Rectangle rectangle49= rectanguloColor2(250,0);
+            Rectangle rectangle50= rectanguloColor1(250,50);
+            Rectangle rectangle51= rectanguloColor2(250,100);
+            Rectangle rectangle52= rectanguloColor1(250,150);
+            Rectangle rectangle53= rectanguloColor2(250,200);
+            Rectangle rectangle54= rectanguloColor1(250,250);
+            Rectangle rectangle55= rectanguloColor2(250,300);
+            Rectangle rectangle56= rectanguloColor1(250,350);
+            Rectangle rectangle57= rectanguloColor2(350,0);
+            Rectangle rectangle58= rectanguloColor1(350,50);
+            Rectangle rectangle59= rectanguloColor2(350,100);
+            Rectangle rectangle60= rectanguloColor1(350,150);
+            Rectangle rectangle61= rectanguloColor2(350,200);
+            Rectangle rectangle62= rectanguloColor1(350,250);
+            Rectangle rectangle63= rectanguloColor2(350,300);
+            Rectangle rectangle64= rectanguloColor1(350,350);
+
+            Group root = new Group(rectangle1,rectangle2,rectangle3,rectangle4,rectangle5,rectangle6,rectangle7,rectangle8,rectangle9,rectangle10,rectangle11,rectangle12,rectangle13,rectangle14,rectangle15,rectangle16,rectangle17,rectangle18,rectangle19,rectangle20,rectangle21,rectangle22,rectangle23,rectangle24,rectangle25,rectangle26,rectangle27,rectangle28,rectangle29,rectangle30,rectangle31,rectangle32,rectangle33,rectangle34,rectangle35,rectangle36,rectangle37,rectangle38,rectangle39,rectangle40,rectangle41,rectangle42,rectangle43,rectangle44,rectangle45,rectangle46,rectangle47,rectangle48,rectangle49,rectangle50,rectangle51,rectangle52,rectangle53,rectangle54,rectangle55,rectangle56,rectangle57,rectangle58,rectangle59,rectangle60,rectangle61,rectangle62,rectangle63,rectangle64);
+        Scene theScene = new Scene(root,400,400);
+        escacs.setScene(theScene);
+        escacs.show();
+    }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
-    public static void main(String[] args) {
-        launch();
+    private Rectangle rectanguloColor1(int X, int Y) {
+        Rectangle rectangle = new Rectangle(50,50);
+        rectangle.setX(X);
+        rectangle.setY(Y);
+        rectangle.setFill(Color.RED);
+        return rectangle;
     }
+
+    private Rectangle rectanguloColor2(int X, int Y) {
+        Rectangle rectangle = new Rectangle(50,50);
+        rectangle.setX(X);
+        rectangle.setY(Y);
+        rectangle.setFill(Color.WHITE);
+        return rectangle;
+    }
+
+    public static void main(String[] args) {launch();}
 }
